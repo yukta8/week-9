ae816f04558e14033ba8798c539895cb5de0e96c
yukta8 <yuktaagarwal8@gmail.com>
2024-01-20 02:24:16 +0530

    action added

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin  #gives access to commands like npm pm2 etc 
 
-cd Week-5-with-axios-and-backend
+# cd Week-5-with-axios-and-backend
+cd week-9
  git pull origin master
  cd server
  pm2 kill
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,2 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+# ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class.pem" ubuntu@ec2-16-171-196-205.eu-north-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
